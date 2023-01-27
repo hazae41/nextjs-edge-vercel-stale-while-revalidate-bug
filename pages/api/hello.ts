7d7ad2d5533315641d354ec1b19d19c8a841322f
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-01-27 12:44:11 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    res.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate=999')
+    res.setHeaders('Cache-Control', 'max-age=0, s-maxage=5, stale-while-revalidate=999')
     return { hello: 'world' }
 }
